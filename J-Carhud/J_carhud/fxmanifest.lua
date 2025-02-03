4c0534b18607c40d97227546ad0755c07040952a
Jasonrp09 <197662903+Jasonrp09@users.noreply.github.com>
2025-02-03 20:05:58 +0100

    Add files via upload

---
+++ J-Carhud/J_carhud/fxmanifest.lua
@@ -0,0 +1,15 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Jason'
+description 'Simple KM/H und Tankanzeige'
+
+ui_page 'ui/index.html'
+
+client_scripts {
+    'client/main.lua'
+}
+
+files {
+    'ui/index.html'
+}

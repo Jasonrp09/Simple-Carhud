4d694b286c877c83a57510d13bd0a5330bb7cece
Emma <151248139+CuzImStupi4@users.noreply.github.com>
2025-02-18 02:31:23 +0100

    Removed useless ESX getSharedObject Event

--- client/main.lua
+++ client/main.lua
@@ -1,37 +1,25 @@
 print("^5Carhud wurde gestartet^7")
 
 
-ESX = nil
-
-Citizen.CreateThread(function()
-    while ESX == nil do
-        TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
-        Citizen.Wait(0)
-    end
-end)
-
--- HUD anzeigen, wenn im Fahrzeug
-Citizen.CreateThread(function()
+CreateThread(function()
     while true do
-        Citizen.Wait(500) -- Überprüft jede halbe Sekunde, ob der Spieler im Fahrzeug ist
+        Wait(500)
         
-        local playerPed = GetPlayerPed(-1)
+        local playerPed = GetPlayerPedId()
         
         if IsPedInAnyVehicle(playerPed, false) then
             local vehicle = GetVehiclePedIsIn(playerPed, false)
-            local speed = GetEntitySpeed(vehicle) * 3.6 -- Geschwindigkeit in km/h
-            local fuel = GetVehicleFuelLevel(vehicle) -- Tankfüllstand
+            local speed = GetEntitySpeed(vehicle) * 3.6
+            local fuel = GetVehicleFuelLevel(vehicle)
 
-            -- HUD anzeigen und Daten senden
             SendNUIMessage({
-                display = true,  -- HUD anzeigen
+                display = true,
                 speed = math.floor(speed),
                 fuel = math.floor(fuel)
             })
         else
-            -- HUD ausblenden, wenn nicht im Fahrzeug
             SendNUIMessage({
-                display = false  -- HUD ausblenden
+                display = false
             })
         end
     end
